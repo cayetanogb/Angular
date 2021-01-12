35e27c0cebc09755b3f00943fdd7499e2472d87c
cayetanogb22 <cayetanogb22@gmail.com>
2021-01-12 11:09:56 +0100

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 import { Component } from '@angular/core';
+import { Model, TodoItem } from "./model";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'todo-app',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'todo';
+  model;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.model = new Model();
+    this.model.filtro = this.model.items;
+  }
+
+  getName() {
+    return this.model.user;
+  }
+
+  getTodoItems() {
+    return this.model.filtro;
+  }
+
+  addItem(newItem) {
+    if (newItem != "") {
+    this.model.items.push(new TodoItem(newItem, false));
+    }
+  }
+
+  botonEliminar(elem: TodoItem) {
+    const indexFound = this.model.items.findIndex((item) => item.action === elem.action);
+
+    if (indexFound >= 0) {
+      this.model.items.splice(indexFound, 1);
+    }
+  }
+
+  filtro(list: string) {
+    let tarea;
+
+    switch (list) {
+      case 'listIncomplete':
+        tarea = this.model.items.filter(item => !item.done);
+        break;
+      case 'listComplete':
+        tarea = this.model.items.filter(item => item.done);
+        break;
+      case 'listTodo':
+        tarea = this.model.items;
+        break;
+    }
+
+    this.model.filtro = tarea;
+  }
 }
